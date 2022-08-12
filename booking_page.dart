4d85f8637f19107f38d62fedcf94409905724bbf
thisura-46 <62584424+thisura-46@users.noreply.github.com>
2022-08-12 18:38:54 +0530

    Add files via upload

---
+++ booking_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class BookingPage extends StatelessWidget {
+  const BookingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Booking Page"),
+    );
+  }
+}
