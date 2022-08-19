ab86e182e6bc8a3cbd70d9b9ff7ea91821cd9805
NethminiNandadewa049 <84173009+NethminiNandadewa049@users.noreply.github.com>
2022-08-19 19:17:06 +0530

    src folder of web application

---
+++ src/components/sideNavbar.vue
@@ -0,0 +1,77 @@
+<template>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <div class="sidebar" :style="{ width: sidebarWidth }">
+    <h1>
+      <span v-if="collapsed">
+        <div>V</div>
+        <div>S</div>
+      </span>
+      <span v-else>Vue Sidebar</span>
+    </h1>
+
+    <SidebarLink to="./HomePage.vue" icon="fas fa-home"></SidebarLink>
+    <SidebarLink to="./dashboardPage" icon="fas fa-columns">Dashboard</SidebarLink>
+    <SidebarLink to="/analytics" icon="fas fa-chart-bar">Analytics</SidebarLink>
+    <SidebarLink to="/friends" icon="fas fa-users">Friends</SidebarLink>
+    <SidebarLink to="/image" icon="fas fa-image">Images</SidebarLink>
+
+    <span
+      class="collapse-icon"
+      :class="{ 'rotate-180': collapsed }"
+      @click="toggleSidebar"
+    >
+      <i class="fas fa-angle-double-left" />
+    </span>
+  </div>
+</template>
+
+<script>
+import '@fortawesome/fontawesome-free/js/all'
+import {collapsed,toggleSidebar,sidebarWidth} from './state'
+
+export default {
+    props:{},
+    setup(){
+        return {collapsed,toggleSidebar,sidebarWidth}
+    },
+    name: 'sideNavbar' 
+}
+</script>
+<style>
+:root{
+    --sidebar-bg-color:#0084BD;
+    --sidebar-item-hover:#0084BD;
+    --sidebar-item-active: #0084BD;
+
+}
+</style>
+<style scoped>
+.sidebar {
+  color: white;
+  background-color: var(--sidebar-bg-color);
+  float: left;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  padding: 0.5em;
+  transition: 0.3s ease;
+  display: flex;
+  flex-direction: column;
+}
+.sidebar h1 {
+  height: 2.5em;
+}
+.collapse-icon {
+  position: absolute;
+  bottom: 0;
+  padding: 0.75em;
+  color: rgba(255, 255, 255, 0.7);
+  transition: 0.2s linear;
+}
+.rotate-180 {
+  transform: rotate(180deg);
+  transition: 0.2s linear;
+}
+</style>
